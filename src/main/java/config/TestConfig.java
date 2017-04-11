cb52d6521d9772b81be2044bb94df77317f6dd57
Ralph Yang <ycj28c@gmail.com>
2017-04-10 17:06:26 -0700

    remove log

--- src/main/java/config/TestConfig.java
+++ src/main/java/config/TestConfig.java
@@ -1,7 +1,5 @@
 package config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +10,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TestConfig {
 
-	private static final Logger log = LoggerFactory.getLogger(TestConfig.class);
-
 	@Bean
 	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
-		log.info("Building PropertyPlaceHolder configurer");
+		System.out.println("Building PropertyPlaceHolder configurer");
 		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
 		ppc.setIgnoreResourceNotFound(false);
 		return ppc;
